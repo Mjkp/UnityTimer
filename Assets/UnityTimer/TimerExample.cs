899b34db9f359662df20efe4a117e88773cfe93a
Mjkp <kpmoon@gmail.com>
2020-07-30 19:35:40 +0900

    first commit

---
+++ Assets/UnityTimer/TimerExample.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimerExample : MonoBehaviour, TimerListener
+{
+
+    public int[] timeInterval; // number and value of timers you want to set 
+    private int initialDelay = 100;
+    private int timerNum; // number of timer
+    const int timerForward = 1;
+    const int timerBackward = 0;
+
+    TimerHandler[] timers;   
+
+    private static void Main()
+    {
+        // main script to activate this script
+
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) InitiateTimer(timerForward);
+    }
+
+    public void Awake()
+    {
+        init();
+    }
+
+    public void init() 
+    {
+
+        //  time intervals;
+        timeInterval = new int[] { 5000,1000 };
+        timerNum = timeInterval.Length;
+
+   
+        timers = new TimerHandler[timerNum];
+        for (int i = 0; i < timerNum; i++)
+        {
+            timers[i] = new TimerHandler();
+        }
+
+        foreach (TimerHandler timer in timers)
+        {
+            timer.AddListener(this);
+        }
+    }
+
+    public void InitiateTimer(int x)
+    {
+        // 0 for foward and 1 for backward
+        if (x == 1)
+        {
+            for (int i = 0; i < timers.Length; i++)
+            {
+                timers[i].StartTimer(initialDelay + timeInterval[(timeInterval.Length-1) - i], "timer-" + i.ToString());
+            }
+        }
+        else if(x ==0)
+        {
+            for (int i = 0; i < timers.Length; i++)
+            {
+                timers[i].StartTimer(initialDelay + timeInterval[i], "timer-" + i.ToString());
+            }
+        }
+    }
+
+
+    // this is the implementation function of the function declared in ITimerListener interface
+    public void OnTimerComplete(object timer)
+    {
+        if(timer == timers[0]) Debug.Log("completed the first timer");
+        if (timer == timers[1]) Debug.Log("completed the second timer");
+        
+
+
+    }
+}
---
+++ Assets/UnityTimer/TimerHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Timers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerHandler
+{
+
+    public float targetTime = 10.0f;
+
+    private Timer aTimer;
+
+    private string name = "defaultTimer";
+
+    private List<TimerListener> listeners = new List<TimerListener>();
+
+    public void AddListener(TimerListener newListener)
+    {
+        listeners.Add(newListener);
+        //Debug.Log("AddListener" + this + " -> " + this.listeners.Count);
+    }
+    public void RemoveListener(TimerListener newListener)
+    {
+        listeners.Remove(newListener);
+    }
+
+    public void StartTimer(int durationMillis,string name)
+    {
+        this.name = name;
+        StartTimer(durationMillis);
+
+    }
+
+    public void StartTimer(int durationMillis)
+    {
+        if(aTimer != null)
+        {
+            aTimer.Enabled = false;
+            aTimer.Close();
+            aTimer.Dispose();
+            aTimer = null;
+            //Debug.Log("existing timer cleared");
+        }
+        aTimer = new System.Timers.Timer(durationMillis);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += (sender, e) => OnTimedEvent(sender, e);
+        aTimer.Enabled = true;
+        //Debug.Log("StartTimer called");
+    }
+
+    private void OnTimedEvent(object source, ElapsedEventArgs e)
+    {
+        //Debug.Log(this + " -> " + this.listeners.Count);
+        aTimer.Stop();
+        for (int i = 0; i < listeners.Count; i++)
+        {
+            TimerListener timerListener = listeners[i];
+            timerListener.OnTimerComplete(this);
+        }
+    }
+
+    public void StopTimer()
+    {
+        aTimer.Stop();
+    }
+
+    public override string ToString()
+    {
+        return "[TestTimer name="+name+"]";
+    }
+
+
+}
